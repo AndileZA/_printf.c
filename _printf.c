bb5eb3e481d4b3561153598e6409e914c0a25813
AndileZA <nangcobo93@gmail.com>
2022-10-19 18:09:26 +0200

    printf

--- _printf.c
+++ _printf.c
@@ -11,28 +11,30 @@ static int (*check_for_specifiers(const char *format))(va_list)
 {
 	unsigned int i;
 	print_t p[] = {
-            {"c", print_c},
-            {"s", print_s},
-	    {"i", print_i},
-	    {"d", print_d},
-	    {"u", print_u},
-	    {"b", print_b},
-	    {"o", print_o},
-	    {"x", print_x},
-	    {"X", print_X},
-	    {"r", print_r},
-            {NULL, NULL}
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
 	};
 
 	for (i = 0; p[i].t != NULL; i++)
-    {
-        if (*(p[i].t) == *format)
-        {
-            break;
-        }
-    }
-    return (p[i].f);
-
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
 }
 
 /**
@@ -41,7 +43,6 @@ static int (*check_for_specifiers(const char *format))(va_list)
  *
  * Return: number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	unsigned int i = 0, count = 0;
@@ -67,12 +68,10 @@ int _printf(const char *format, ...)
 			i += 2;
 			continue;
 		}
-
 		if (!format[i + 1])
 			return (-1);
 		_putchar(format[i]);
 		count++;
-
 		if (format[i + 1] == '%')
 			i += 2;
 		else
